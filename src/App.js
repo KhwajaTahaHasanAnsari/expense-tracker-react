a9358fec76502be09a00fc570c3bbe8994f38d5c
Taha H khwaja <KhwajaTahaHasanAnsari@users.noreply.github.com>
2020-06-20 22:05:49 +0500

    add history componet ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Header} from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList  } from './components/TransactionList';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
      <Header />
      <Balance />
      <IncomeExpenses />
+     <TransactionList />
     </div>
   );
 }
